c7cb4c2aae913cf999554285af8b662c2f50faec
emiliano-ma <emilianomainero@gmail.com>
2020-10-01 12:29:38 +0200

    Formatting

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,52 +1,49 @@
-import React, { Component } from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Button, Input } from "semantic-ui-react";
 import { getSearch } from "../modules/searchGh";
 
 export class GHSearch extends Component {
   state = {
     searchResult: [],
-    inputField: ''
-  }
+    inputField: "",
+  };
 
   search = async (e) => {
-    let input = this.state.inputField
+    let input = this.state.inputField;
     let result = await getSearch(input);
-    debugger
+    debugger;
     this.setState({ searchResult: result });
-  }
+  };
 
   render() {
-
     let searchResultList;
-    
-    if (this.state.searchResult.length > 0) {
 
+    if (this.state.searchResult.length > 0) {
       searchResultList = this.state.searchResult.items.map((item) => {
         return (
           <div data-id={item.id} data-cy={"search-result-" + item.id}>
             <h3>{item.login}</h3>
-            
-            {/* {product.id == parseInt(this.state.orderResponse.id) && (
-              <p id="message">{this.state.orderResponse.message}</p>
-            )} */}
           </div>
         );
-    })
-  }
-
-    
-
+      });
+    }
 
     return (
       <>
-        <Input onChange={(e) =>  this.setState({inputField: e.target.value})} data-cy="username" type="text" name="search" placeholder="Input GH username"/>
-        <Button onClick={this.search} data-cy="search" name="search">Search</Button>
+        <Input
+          onChange={(e) => this.setState({ inputField: e.target.value })}
+          data-cy="username"
+          type="text"
+          name="search"
+          placeholder="Input GH username"
+        />
+        <Button onClick={this.search} data-cy="search" name="search">
+          Search
+        </Button>
         {searchResultList}
       </>
-    )
+    );
   }
 }
 
-export default GHSearch
-
-
+export default GHSearch;
