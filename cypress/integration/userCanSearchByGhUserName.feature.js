bab8fac4b6258272befe58f292125b6420a33135
emiliano-ma <emilianomainero@gmail.com>
2020-10-01 11:18:30 +0200

    Adding User story to the writtenExam file. Adding acceptance test for user can search by GH Username and get a list of results. Adding ficture file to stub the response from GH API

---
+++ cypress/integration/userCanSearchByGhUserName.feature.js
@@ -0,0 +1,21 @@
+describe("User can search by GH user name", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "https://api.github.com/search/users?q=emiliano-ma",
+      response: "fixture:search_result.json",
+    });
+    cy.visit("/");
+  });
+
+  it("User can type a user name and get a search result as a response when clicking 'Search'", () => {
+    cy.get('[data-cy="username"]').type('emiliano-ma');
+    cy.get('[data-cy="search"]').click();
+
+    cy.get("[data-cy=search-result]").within(() => {
+      cy.get("[data-cy=search-result-1]").should("contain", "emiliano-ma")
+    });
+  });
+
+});
--- writtenExam.md
+++ writtenExam.md
@@ -3,3 +3,12 @@
 How and when to use `.forEach`, `.map`, `.filter`, and `.find` in JavaScript?
 
 Please give an as extensive explanation as possible with code examples and potential use cases. Write your answer below and use markdown to format your text.
+
+
+
+----
+User stories:
+
+As a user of the GH searching app
+in order to find GH users
+I'd like to type a GH user name and get a list of matching user names available in GH
